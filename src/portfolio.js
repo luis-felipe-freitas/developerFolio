f2c3336e1bed0ef9cefe213b633ccc920df9b05c
giovannabbottino <giovannabbottino@gmail.com>
2021-12-09 10:00:14 -0300

    Pequena modificacao no greeting e header

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,10 +11,12 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Luis Felipe Freitas",
+  title: "Hello there, I'm Luis Felipe Freitas",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    `A passionate Full Stack Software Developer 🚀 having an experience 
+    of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / 
+    React Native and some other cool libraries and frameworks.`
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
